32165c5e13d2b30213cb21c045f29c72e2df3dc4
NIKHIL SHARMA <nikhilsharma101001@gmail.com>
2023-10-15 18:14:51 +0530

    first commit

---
+++ utils/index.js
@@ -0,0 +1,11 @@
+export const checkImageUrl = (url) => {
+  if (!url) {
+    return false;
+  } else {
+    const pattern = new RegExp(
+      "^https?:\\/\\/.+\\.(jpg|jpeg|png|gif|bmp|webp)$",
+      "i"
+    );
+    return pattern.test(url);
+  }
+};
